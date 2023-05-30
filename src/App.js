1157a8849f1c88dc3b9bdacabcf600a45d31b305
TMZSeif <seiftamer.faresnegm@gmail.com>
2023-05-30 19:15:49 +0300

    added more styling

--- src/App.js
+++ src/App.js
@@ -2,16 +2,17 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import './App.css'
 
 const App = (props) => {
   return (
-    <Container id="quote-box">
+    <Container className="quote-box" id="quote-box">
       <h2 id="text">{props.quote}</h2>
       <br></br>
       <Row>
-        <Col xs={9} ><p id="author">{props.author}</p></Col>
-        <Col><button id="new-quote">New Quote</button></Col>
-        <Col><a rel="noreferrer" target='_blank' href={"twitter.com/intent/tweet?text="+props.quote} id="tweet-quote">Tweet Quote</a></Col>
+        <Col xs={6} ><p id="author">{props.author}</p></Col>
+        <Col xs="auto" ><button id="new-quote">New Quote</button></Col>
+        <Col xs="auto" ><a rel="noreferrer" target='_blank' href={"twitter.com/intent/tweet?text="+props.quote} id="tweet-quote">Tweet Quote</a></Col>
       </Row>
     </Container>
   );
