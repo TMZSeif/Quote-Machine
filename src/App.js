a9d2988ce5e2e887f0e579908818d4c878a56662
TMZSeif <seiftamer.faresnegm@gmail.com>
2023-05-30 19:39:15 +0300

    added new quote functionality and styling

--- src/App.js
+++ src/App.js
@@ -2,20 +2,39 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
 import './App.css'
+import getapi from "./features/quote-api/api.js"
 
-const App = (props) => {
-  return (
-    <Container className="quote-box" id="quote-box">
-      <h2 id="text">{props.quote}</h2>
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      quote: this.props.quote,
+      author: this.props.author
+    }
+    this.newQuote = this.newQuote.bind(this);
+  }
+
+  async newQuote() {
+    const quote = await getapi()
+    this.setState({
+      quote: await quote[0].quote,
+      author: await quote[0].author
+    })
+  }
+
+  render() {
+    return <Container className="quote-box" id="quote-box">
+      <h2 id="text">{this.state.quote}</h2>
       <br></br>
       <Row>
-        <Col xs={6} ><p id="author">{props.author}</p></Col>
-        <Col xs="auto" ><button id="new-quote">New Quote</button></Col>
-        <Col xs="auto" ><a rel="noreferrer" target='_blank' href={"twitter.com/intent/tweet?text="+props.quote} id="tweet-quote">Tweet Quote</a></Col>
+        <Col xs={8} ><p id="author">{this.state.author}</p></Col>
+        <Col xs="auto" ><Button onClick={this.newQuote} variant="primary" id="new-quote">New Quote</Button></Col>
+        <Col xs="auto" ><Button variant="primary" rel="noreferrer" target='_blank' href={"https://twitter.com/intent/tweet?text="+this.state.quote} id="tweet-quote">Tweet Quote</Button></Col>
       </Row>
     </Container>
-  );
+  };
 }
 
 export default App;
